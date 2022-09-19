9333827b7c2a424e006cf799326383ef0a02145d
Illia <illya1004work@gmail.com>
2022-09-19 11:59:43 +0100

    feat: added update restaurant mutation

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -2,16 +2,19 @@ import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { Restaurant } from './entities/restaurant.entity';
 import { CreateRestaurantDto } from './dto/create-restaurant.dto';
 import { RestaurantsService } from './restaurants.service';
+import { UpdateRestaurantDto } from './dto/update-restaurant.dto';
 
 // added entity type
 @Resolver(() => Restaurant)
 export class RestaurantsResolver {
   constructor(private readonly restaurantService: RestaurantsService) {}
+
   // this decorator for generate schema
   @Query(() => [Restaurant])
   restaurants(): Promise<Restaurant[]> {
     return this.restaurantService.getAll();
   }
+
   @Mutation(() => Boolean)
   async createRestaurant(
     @Args('input') createRestaurantInput: CreateRestaurantDto,
@@ -23,5 +26,12 @@ export class RestaurantsResolver {
       console.log(e);
       return false;
     }
+  }
+
+  @Mutation(() => Boolean)
+  async updateRestaurant(
+    @Args('input') updateRestaurantDto: UpdateRestaurantDto,
+  ) {
+    return true;
   }
 }
